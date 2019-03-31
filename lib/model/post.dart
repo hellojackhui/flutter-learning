c3824c3b2e932ddee95a272ab11cfba4bd77b7ad
hellojackhui <huijiawe.i@163.com>
2019-03-31 09:15:22 +0800

    demo-1

---
+++ lib/model/post.dart
@@ -0,0 +1,30 @@
+class Post {
+  const Post({
+    this.title,
+    this.author,
+    this.imageUrl,
+  });
+
+  final String title;
+  final String author;
+  final String imageUrl;
+
+}
+
+final List<Post> posts = [
+  Post(
+    title: 'jackhui',
+    author: 'jackhui111',
+    imageUrl: 'http://pic31.nipic.com/20130720/5793914_122325176000_2.jpg'
+  ),
+  Post(
+    title: 'car',
+    author: 'jackhui111123',
+    imageUrl: 'http://img1.xcarimg.com/exp/2872/2875/2937/20101220130509576539.jpg'
+  ),
+  Post(
+    title: 'background',
+    author: 'jackhui11112312',
+    imageUrl: 'http://www.pptok.com/wp-content/uploads/2012/08/xunguang-4.jpg'
+  )
+];
