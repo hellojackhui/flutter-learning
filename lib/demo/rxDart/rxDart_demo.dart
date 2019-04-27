3d1f86787faf74a04ea5f342622e2b1a6cb33e30
hellojackhui <huijiawe.i@163.com>
2019-04-27 14:27:05 +0800

    rxDart finish

--- lib/demo/rxDart/rxDart_demo.dart
+++ lib/demo/rxDart/rxDart_demo.dart
@@ -22,20 +22,61 @@ class RxDartDemoHome extends StatefulWidget {
 }
 
 class _RxDartDemoHomeState extends State<RxDartDemoHome> {
+
+  PublishSubject<String> _textFieldSubject;
+
+
   @override
   void initState() {
     super.initState();
-    Observable<String> _observable = 
+
+    _textFieldSubject = PublishSubject<String>();
+
+    _textFieldSubject
+    .debounce(Duration(microseconds: 500))
+    .listen((data) =>print(data));
+
+    // Observable<String> _observable = 
     // Observable(Stream.fromIterable(['hello', 'jackhui']));
     // Observable.fromFuture(Future.value('hello'));
     // Observable.fromFuture(Future.delayed(Duration(seconds: 5)));
-    Observable.periodic(Duration(seconds: 3), (x) => x.toString());
-    _observable.listen(print);
+    // Observable.periodic(Duration(seconds: 3), (x) => x.toString());
+    // _observable.listen(print);
+
+    // PublishSubject<String> _subject = PublishSubject<String>();  每一个数据智能被监听一次
+    // BehaviorSubject<String> _subject = BehaviorSubject<String>();  将最后的add作为消息监听的数据，最后的数据被所有监听器监听
+    // ReplaySubject<String> _subject = ReplaySubject<String>(); // 将所有监听的数据都交给observable，每个数据都会被所有监听器监听一次
+    
+    // _subject.add('hello');
+    // _subject.add('hell02');
+    // _subject.listen((data) => print('listen 1: $data'));
+    // _subject.listen((data) => print('listen 2: ${data.toUpperCase()}'));
+    // _subject.close();
+  }
+
+  @override
+  void dispose() {
+    _textFieldSubject.close();
+    super.dispose();
   }
   @override
   Widget build(BuildContext context) {
-    return Container(
-
+    return Theme(
+      data: Theme.of(context).copyWith(
+        primaryColor: Colors.black,
+      ),
+      child: TextField(
+        onChanged: (value) {
+          _textFieldSubject.add('input: $value');
+        },
+        onSubmitted: (value) {
+          _textFieldSubject.add('submit: $value');
+        },
+        decoration: InputDecoration(
+          labelText: 'Title',
+          filled: true
+        ),
+      ),
     );
   }
 }
